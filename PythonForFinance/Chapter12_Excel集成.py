f723576971e5b9955ff7002ba731607e9cf87c79
GaobinWang <gaobin513@qq.com>
2017-10-05 23:39:57 +0800

    Excel集成

---
+++ PythonForFinance/Chapter12_Excel集成.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 23:33:03 2017
+
+@author: Lesile
+"""
+
+"""
+###Chapter12 Excel集成
+Python与Excel集成可以起到以下作用:
+    操纵工具:使用Python可以与Excel电子表格交互并加以操纵
+    数据处理器:Python可以向电子表格提供数据,从电子表格处理数据
+    分析引擎:Python可以向电子表格提供完整的分析能力，称为VBA编程的成熟替代品
+"""
+
+"""
+###12.1 基本电子表格交互
+"""
+
+"""
+###12.2 用Python编写Excel脚本
+"""
+
+"""
+###12.3 xlwings
+"""
+
+
