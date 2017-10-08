ecf6c2ba2bb953daabf866a1cdfdfae5b665604c
GaobinWang <gaobin513@qq.com>
2017-10-08 17:51:49 +0800

    PyMC3

---
+++ PythonForFinance/Chapter11_统计学/Chapter11_统计学.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 17:17:38 2017
+
+@author: Lesile
+"""
+
+"""
+Chapter11 统计学
+"""
+
+"""
+###PyMC3
+"""
+###A Motivating Example: Linear Regression
+#产生数据
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Initialize random number generator
+np.random.seed(123)
+
+# True parameter values
+alpha, sigma = 1, 1
+beta = [1, 2.5]
+
+# Size of dataset
+size = 100
+
+# Predictor variable
+X1 = np.random.randn(size)
+X2 = np.random.randn(size) * 0.2
+
+# Simulate outcome variable
+Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+
+%matplotlib inline
+
+fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,4))
+axes[0].scatter(X1, Y)
+axes[1].scatter(X2, Y)
+axes[0].set_ylabel('Y'); axes[0].set_xlabel('X1'); axes[1].set_xlabel('X2');
+
+#模型假定
+import pymc3 as pm
+basic_model = pm.Model()
+
+with basic_model:
+
+    # Priors for unknown model parameters
+    alpha = pm.Normal('alpha', mu=0, sd=10)
+    beta = pm.Normal('beta', mu=0, sd=10, shape=2)
+    sigma = pm.HalfNormal('sigma', sd=1)
+
+    # Expected value of outcome
+    mu = alpha + beta[0]*X1 + beta[1]*X2
+
+    # Likelihood (sampling distribution) of observations
+    Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=Y)
+
+
+#模型拟合
+#Maximum a posteriori methods
+map_estimate = pm.find_MAP(model=basic_model)
+
+map_estimate
+
+from scipy import optimize
+
+map_estimate = pm.find_MAP(model=basic_model, fmin=optimize.fmin_powell)
+
+map_estimate
+
+
+
+
+
